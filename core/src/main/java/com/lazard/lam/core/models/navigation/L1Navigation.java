f4f274ed89d3615cb489bf63b7268fd106a2bb0c
Kayle Drucker <Kayle.Drucker@lazard.com>
2024-05-08 15:23:49 -0500

    Removed unnecessary imports to the java files and all logs. In the java files, I added the list of children based on the root page path. Then created the NavigationItem java file to capture the child's title, path, page properties (body, infoDate, readTime, and image).

--- core/src/main/java/com/lazard/lam/core/models/navigation/L1Navigation.java
+++ core/src/main/java/com/lazard/lam/core/models/navigation/L1Navigation.java
@@ -1,49 +1,27 @@
 package com.lazard.lam.core.models.navigation;
 
-
-
 import lombok.Getter;
+
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
+
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
-import org.apache.sling.models.annotations.injectorspecific.SlingObject;
-import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Getter
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class L1Navigation {
 
-    @SlingObject
-    private Resource resource;
-
     private static final Logger LOG = LoggerFactory.getLogger(L1Navigation.class);
-
-    @Getter
-    @ValueMapValue
-    public String text;
 
     @ChildResource
     public List<L1NavigationItem> l1navItems;
-
-    @PostConstruct
-    private void init() {
-        LOG.error("---------------------------------------");
-        LOG.error("You are in L1 Nav");
-        LOG.error("---------------------------------------");
-    }
-
-    public boolean isTextEmpty() {
-        return StringUtils.isEmpty(this.text);
-    }
 
     public boolean isNavItemEmpty() {
         return CollectionUtils.isEmpty(this.l1navItems);
--- core/src/main/java/com/lazard/lam/core/models/navigation/L1NavigationItem.java
+++ core/src/main/java/com/lazard/lam/core/models/navigation/L1NavigationItem.java
@@ -4,15 +4,18 @@ import com.day.cq.wcm.api.Page;
 
 import lombok.Getter;
 
-import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
-import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,15 +27,6 @@ import javax.annotation.PostConstruct;
 public class L1NavigationItem {
 
     private static final Logger LOG = LoggerFactory.getLogger(L1Navigation.class);
-
-    @ScriptVariable
-    protected Page currentPage;
-
-    @Self
-    private SlingHttpServletRequest request;
-
-    @Self
-    private Resource resource;
 
     @SlingObject
     private ResourceResolver resourceResolver;
@@ -48,23 +42,59 @@ public class L1NavigationItem {
     @Getter
     private String title;
 
+    @Getter
+    private int l1Level = 1;
+
+    @Getter
+    private List<NavigationItem> listItems = new ArrayList<>();
+
     @PostConstruct
     public void init() {
-        this.title = this.getPageTitle();
-    }
-
-    public String getPageTitle() {
-        Resource res = resourceResolver.getResource(this.navigationRoot);
-        String pageTitle = "";
-        if(res != null) {
-            Page page = res.adaptTo(Page.class);
-            if (page != null) {
-                pageTitle = page.getTitle();
+        Resource resource = getResource(this.navigationRoot);
+        if (resource != null){
+            Page rootPage = resource.adaptTo(Page.class);
+            if (rootPage != null){
+                int startLevel = rootPage.getDepth();
+                this.title = rootPage.getTitle();
+                this.setChildrenList(startLevel, rootPage);
             }
         }
-        return pageTitle;
     }
 
+    public Resource getResource(String path){
+        return resourceResolver.getResource(path);
+    }
 
+    public void setChildrenList(int startLevel, Page parent) {
+        Iterator<Page> listChildren = parent.listChildren(null, true);
+        NavigationItem newItem;
+        while (listChildren.hasNext()) {
+            Page currentChild = listChildren.next();
+            int level = (currentChild.getDepth() - startLevel) + 1;
+            String body = this.getProperties(currentChild, "jcr:description");
+            String infoDate = this.getProperties(currentChild, "infoDate"); //Type to be updated when page properties is added to the template
+            String readTime = this.getProperties(currentChild, "readTime"); //Type to be updated when page properties is added to the template
+            String image = this.getProperties(currentChild, "image"); //Type to be updated when page properties is added to the template
+            newItem = new NavigationItem(currentChild.getTitle(),
+                    currentChild.getPath(),
+                    level,
+                    body,
+                    infoDate,
+                    readTime,
+                    image);
+            listItems.add(newItem);
+        }
+    }
 
+    public String getProperties(Page parent,String property) {
+        ValueMap properties = parent.getProperties();
+        if (properties.containsKey(property)){
+            return properties.get(property).toString();
+        }
+        return null;
+    }
+
+    public boolean isListItemsEmpty() {
+        return CollectionUtils.isEmpty(this.listItems);
+    }
 }
